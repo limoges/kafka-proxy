148100a90384a395d4778b89eaccadbf32cdd406
Maksim Zhylinski <maksim.zhylinski@sumup.com>
2025-07-10 18:26:40 +0200

    Revert "Enable pprof"
    This reverts commit c8c503acaddd96be6f02d1035c8aa6c2a6928f26.

--- main.go
+++ main.go
@@ -5,11 +5,7 @@ import (
 	"github.com/grepplabs/kafka-proxy/cmd/kafka-proxy"
 	"github.com/grepplabs/kafka-proxy/cmd/tools"
 	"github.com/spf13/cobra"
-	"log"
-	"net/http"
 	"os"
-
-	_ "net/http/pprof"
 )
 
 var RootCmd = &cobra.Command{
@@ -29,10 +25,6 @@ func init() {
 }
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
-
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
